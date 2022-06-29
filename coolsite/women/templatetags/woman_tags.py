87c35bc60598aae5abb5c1fa4ede8683685bee0b
marchell93 <69196596+marchell93@users.noreply.github.com>
2022-06-29 12:17:06 +0300

    Selfedu Django lesson 19

--- coolsite/women/templatetags/woman_tags.py
+++ coolsite/women/templatetags/woman_tags.py
@@ -28,7 +28,6 @@ def show_menu():
     menu = [{'title': 'О сайте', 'url_name': 'about'},
             {'title': 'Добавить статью', 'url_name': 'add_page'},
             {'title': 'Обратная связь', 'url_name': 'contact'},
-            {'title': 'Войти', 'url_name': 'login'},
             ]
 
     return {'menu': menu}
